e00a38909fd12663583f3b5c1a0527dbb8912340
Fibii <charafbahria@gmail.com>
2019-12-20 20:20:17 +0100

    [frontend][all] fixed eslint, code reformat

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,17 +13,25 @@ module.exports = {
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    'react',
+  ],
   rules: {
     "semi": [
       "error",
       "never"
     ],
+    "react/jsx-filename-extension": [0],
     "import/extensions": "off",
     "no-console": "off",
     "no-alert": "off",
     "no-shadow": "off",
+    "react/prop-types": "off"
   },
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button'
-import SignUpForm from "./components/signupForm"
+import SignUpForm from './components/signupForm'
 
 const App = () => {
   return (
-      <SignUpForm />
+    <SignUpForm/>
   )
 }
 
