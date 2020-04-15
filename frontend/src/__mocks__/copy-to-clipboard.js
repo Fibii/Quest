3bc3c9b48af0f93046bead0abedb88bd24d25107
Fibii <charafbahria@gmail.com>
2020-04-15 23:54:36 +0100

    [frontend][tests] add QuestionIcons tests (failing)

---
+++ frontend/src/__mocks__/copy-to-clipboard.js
@@ -0,0 +1,5 @@
+/**
+ * A mock for CopyToClipboard component used by QuestionIcons
+ * */
+const copy = jest.fn()
+export default copy
---
+++ frontend/src/__tests__/QuestionIcons.test.js
@@ -0,0 +1,110 @@
+import { render, fireEvent } from '@testing-library/react'
+import React from 'react'
+import QuestionIcons from '../components/PartialViews/QuestionIcons'
+
+describe('QuestionIcons tests', () => {
+  /**
+   * First state is initialized to nothing, when the edit button is clicked,
+   * it should be updated to 'UPDATED' and so on
+   * */
+  test('renders QuestionIcons and executes callback', async () => {
+    let state = 'NOTHING'
+    const EDITED = 'EDITED'
+    const DELETED = 'DELETED'
+    const UPDATED = 'UPDATED'
+    const path = '/tests/question/icons'
+    let alertOpen = false
+    // jsdom doesn't implement window.prompt, implement it as an empty object
+    // so jsdom doesn't throw an error
+    window.prompt = () => {}
+    const alertSetOpen = (value) => {
+      alertOpen = value
+    }
+
+    /**
+     * Function that gets called when one of the question is clicked, used to
+     * update state
+     * @param newState: the new state after a button is clicked, one of:
+     *  - SHARED
+     *  - EDITED
+     *  - DELETED
+     *  - UPDATED
+     * */
+    const update = (newState) => {
+      state = newState
+    }
+
+    const { getByTestId, rerender } = render(
+      <QuestionIcons
+        direction="row"
+        handleUpdate={() => update(UPDATED)}
+        handleDelete={() => update(DELETED)}
+        handleEdit={() => update(EDITED)}
+        path={path}
+        alertOpen={alertOpen}
+        alertSetOpen={alertSetOpen}
+        alertCallback={() => update(DELETED)}
+      />,
+    )
+
+    fireEvent.click(getByTestId('edit-button'))
+    expect(state).toBe(EDITED)
+    expect(alertOpen).toBeFalsy()
+    // click the update button
+    fireEvent.click(getByTestId('update-button'))
+    // expect alertWindow to be open
+    expect(alertOpen).toBeTruthy()
+    expect(state).toBe(EDITED)
+    // rerender to get get the opened alertWindow into the dom
+    rerender(
+      <QuestionIcons
+        direction="row"
+        handleUpdate={() => update(UPDATED)}
+        handleDelete={() => update(DELETED)}
+        handleEdit={() => update(EDITED)}
+        path={path}
+        alertOpen={alertOpen}
+        alertSetOpen={alertSetOpen}
+        alertCallback={() => update(DELETED)}
+      />,
+    )
+    // click confirm button on alertWindow
+    fireEvent.click(getByTestId('confirm-button'))
+    expect(state).toBe(UPDATED)
+
+    // click the delete button
+    fireEvent.click(getByTestId('delete-button'))
+    // expect alertWindow to be open
+    expect(alertOpen).toBeTruthy()
+    expect(state).toBe(UPDATED)
+    // click confirm button on alertWindow
+    rerender(
+      <QuestionIcons
+        direction="row"
+        handleUpdate={() => update(UPDATED)}
+        handleDelete={() => update(DELETED)}
+        handleEdit={() => update(EDITED)}
+        path={path}
+        alertOpen={alertOpen}
+        alertSetOpen={alertSetOpen}
+        alertCallback={() => update(DELETED)}
+      />,
+    )
+    fireEvent.click(getByTestId('confirm-button'))
+    expect(state).toBe(DELETED)
+    rerender(
+      <QuestionIcons
+        direction="row"
+        handleUpdate={() => update(UPDATED)}
+        handleDelete={() => update(DELETED)}
+        handleEdit={() => update(EDITED)}
+        path={path}
+        alertOpen={alertOpen}
+        alertSetOpen={alertSetOpen}
+        alertCallback={() => update(DELETED)}
+      />,
+    )
+    fireEvent.click(getByTestId('share-button'))
+    expect(getByTestId('snackbar').textContent).toContain('copied to clipboards')
+  })
+})
