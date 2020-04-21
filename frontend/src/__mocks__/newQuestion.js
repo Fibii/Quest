cc14cf3c790bb50a9e5ad006afdf9dab635743ae
Fibii <charafbahria@gmail.com>
2020-04-22 00:34:53 +0100

    [frontend][tests] add missing newQuestion mock

---
+++ frontend/src/__mocks__/newQuestion.js
@@ -0,0 +1,16 @@
+const newQuestion = {
+  tags: ['question', 'test', 'tag'],
+  comments: [],
+  title: 'question test title',
+  content: 'question test content',
+  solved: false,
+  likes: [],
+  postedDate: Date.now(),
+  postedBy: {
+    username: 'fibi',
+    id: '5e8e330f42b04e2f7dbe2224',
+  },
+  id: '5e90f2b22370477f68d95fce',
+}
+
+export default newQuestion
