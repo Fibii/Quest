986c99fabddc954372075906b8e5596743799794
Fibii <charafbahria@gmail.com>
2020-04-25 23:09:34 +0100

    [frontend][tests] add Notification test

---
+++ frontend/src/__tests__/Notification.test.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import Notification from '../components/Notification'
+
+describe('Notification Tests', () => {
+  test('renders title and message', () => {
+    const title = 'Error'
+    const message = 'an internal error has occurred'
+    const { getByTestId } = render(
+      <Notification title={title} message={message} />,
+    )
+    expect(getByTestId('title').textContent).toEqual(title)
+    expect(getByTestId('message').textContent).toEqual(message)
+  })
+})
