20df947e96b388ae1d896b938da4c116127050c2
Fibii <charafbahria@gmail.com>
2020-05-14 23:51:28 +0100

    [frontend][tests] add missing comment mock

---
+++ frontend/src/__mocks__/comment.js
@@ -0,0 +1,20 @@
+const comment = {
+  content: 'another comment by fibi',
+  likes: [
+    {
+      _id: '5e76c5b0bdd6e639a05e3d15',
+      value: 0,
+    },
+    {
+      _id: '5e875c2aa876bf63d94cd5ff',
+      value: 1,
+      likedBy: '5e875947a876bf63d94cd5fe',
+    },
+  ],
+  postedBy: {
+    id: '5e8e330f42b04e2f7dbe2224',
+  },
+  id: '5e76c5b0bdd6e639a05e3d14',
+}
+
+export default comment
