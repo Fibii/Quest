da6c771a58ee26b985d6893cad9ce50ef4f4a61f
Fibii <charafbahria@gmail.com>
2020-06-04 20:59:40 +0100

    [F][C] add Copyrights to MainApp, add minHeight styling to fix footer overlap in small resolutions

--- frontend/src/components/MainApp/MainApp.js
+++ frontend/src/components/MainApp/MainApp.js
@@ -3,6 +3,7 @@ import {
   Route, Switch, useLocation, useHistory,
 } from 'react-router-dom'
 import grey from '@material-ui/core/colors/grey'
+import { makeStyles } from '@material-ui/core/styles'
 import UserContext from '../UserContext/UserContext'
 
 import Header from '../Header/Header'
@@ -18,11 +19,23 @@ import userService from '../../services/users'
 import { setErrorMessage } from '../../actions/questionActions'
 import Profile from '../Profile/Profile'
 import Notification from '../Notification/Notification'
+import Copyrights from '../Copyrights/Copyrights'
+
+const useStyles = makeStyles(() => ({
+  container: {
+    backgroundColor: grey[100],
+    height: '100vh',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+}))
 
 const MainApp = () => {
   const [user, setUser] = useState(null)
   const location = useLocation()
   const history = useHistory()
+  const classes = useStyles()
 
   useEffect(() => {
     try {
@@ -51,13 +64,7 @@ const MainApp = () => {
   }
 
   return (
-    <div
-      style={{
-        backgroundColor: grey[100],
-        height: '100vh',
-      }}
-      data-testid="mainApp-container"
-    >
+    <div data-testid="mainApp-container" className={classes.container}>
       <UserContext.Provider value={[user, setUser]}>
         <Header />
         <Switch>
@@ -79,6 +86,7 @@ const MainApp = () => {
           <Route path="/question/:id" exact render={() => <Question />} />
           <Route path="/user/:id" exact render={() => <Profile />} />
         </Switch>
+        <Copyrights />
       </UserContext.Provider>
     </div>
   )
