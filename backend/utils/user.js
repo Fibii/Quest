7274e2c08052c2b26135c8e97b2225de504af161
Fibii <charafbahria@gmail.com>
2020-08-23 16:33:13 +0100

    [B][U][user] add userId to isAuthenticated
    to authenticate User "Modify" operations

--- backend/utils/user.js
+++ backend/utils/user.js
@@ -3,11 +3,18 @@ const User = require('../models/user')
 
 /**
  * Checks if a user is logged in or not
- * @param id: mongodb id of the user
- * @param user: the token the user used in the request
+ *
+ * If userId is undefined, that's because it's not needed to
+ * authenticate the user, like if the user is posting a new question
+ *
+ * If a user is updating/deleting a User object, then userId must be defined to make sure
+ * that the token provided in the request belongs to the user with userId
+ *
+ * @param userId: mongodb id of the user
+ * @param token: the token the user used in the request
  * @return user object if the user is authenticated, error object otherwise
  * */
-const isAuthenticated = async (token) => {
+const isAuthenticated = async (token, userId) => {
   const decodedToken = jwt.decode(token, process.env.SECRET)
   const errorObj = { error: 'token missing or invalid' }
 
@@ -15,7 +22,12 @@ const isAuthenticated = async (token) => {
     return errorObj
   }
 
-  const user = await User.findById(decodedToken.id)
+  if (userId && (!decodedToken.id || decodedToken.id !== userId)) {
+    return errorObj
+  }
+
+  const id = userId || decodedToken.id
+  const user = await User.findById(id)
 
   if (!user) {
     return errorObj
