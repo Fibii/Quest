4c0ed525d841703304df22571c275e424e390fd9
Fibii <charafbahria@gmail.com>
2020-09-12 13:24:19 +0100

    [F][S] read baseUrl from .env instead of hard-coding it

--- frontend/src/services/questions.js
+++ frontend/src/services/questions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api'
+const baseUrl = process.env.REACT_APP_BACKEND_URL
 let token = null
 let config = null
 
--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api'
+const baseUrl = process.env.REACT_APP_BACKEND_URL
 // eslint-disable-next-line no-unused-vars
 let token = null
 
