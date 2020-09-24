6a2ef79d6c58a9c61a200a8703e36171de995668
Fibii <charafbahria@gmail.com>
2020-09-24 16:27:22 +0100

    [B] use .env, add execution info

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,12 @@ const http = require('http')
 const app = require('./app')
 
 const server = http.createServer(app)
-const { PORT } = process.env
+const { PORT } = process.env || 3000
+
+if (!PORT) {
+  console.log("no port was provided in .env, port 3000 will be used by default")
+}
+
 server.listen(PORT, () => {
   console.log(`server started on localhost:${PORT}`)
 })
