2edcf5553255986aae7574bea10b2f25e772e196
Fibii <charafbahria@gmail.com>
2021-02-18 17:39:52 +0100

    yeah

--- frontend/src/components/MainApp/MainApp.js
+++ frontend/src/components/MainApp/MainApp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, lazy, Suspense } from 'react'
 import {
   Route, Switch,
 } from 'react-router-dom'
@@ -6,13 +6,12 @@ import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import UserContext from '../UserContext/UserContext'
 
-import Header from '../Header/Header'
-import Welcome from '../Welcome/Welcome'
-import SignIn from '../SignInForm/SignInForm'
-import SignupForm from '../SignupForm/SignupForm'
+// import Header from '../Header/Header'
+// import SignIn from '../SignInForm/SignInForm'
+// import SignupForm from '../SignupForm/SignupForm'
 import NewQuestionForm from '../NewQuestionForm/NewQuestionForm'
 import Question from '../Question/Question'
-import Questions from '../Questions/Questions'
+// import Questions from '../Questions/Questions'
 import userService from '../../services/users'
 import { setErrorMessage } from '../../actions/questionActions'
 import Profile from '../Profile/Profile'
@@ -33,6 +32,14 @@ const useStyles = makeStyles(() => ({
     flexShrink: 0,
   },
 }))
+
+const Welcome = lazy(() => import('../Welcome/Welcome'))
+const SignIn = lazy(() => import('../SignInForm/SignInForm'))
+const SignupForm = lazy(() => import('../SignupForm/SignupForm'))
+const Questions = lazy(() => import('../Questions/Questions'))
+const Header = lazy(() => import('../Header/Header'))
+// const Welcome = lazy(() => import('../Welcome/Welcome'))
+// const Welcome = lazy(() => import('../Welcome/Welcome'))
 
 const MainApp = () => {
   const [user, setUser] = useState(null)
@@ -63,29 +70,41 @@ const MainApp = () => {
   return (
     <Grid container direction="column" className={classes.container} wrap="nowrap">
       <UserContext.Provider value={[user, setUser]}>
-        <Header />
-        <Grid className={classes.content}>
-          <Switch>
-            <Route
-              exact
-              path={urls.root}
-              render={() => (
-                user ? (
-                  <Questions user={user} />
-                ) : (
+          <Header />
+          <Grid className={classes.content}>
+            <Switch>
+              <Route
+                exact
+                path={urls.root}
+                render={() => (
+                  user ? (
+                    <Suspense fallback="loading">
+                      <Questions user={user} />
+                    </Suspense>
+                  ) : (
+                    <Welcome />
+                  )
+                )}
+              />
+              <Route path={urls.welcome} render={() =>
+                <Suspense fallback="loading">
                   <Welcome />
-                )
-              )}
-            />
-            <Route path={urls.welcome} render={() => <Welcome />} />
-            <Route path={urls.login} render={() => <SignIn setUser={setUser} />} />
-            <Route path={urls.register} component={SignupForm} />
-            <Route path={urls.newQuestion} exact render={() => <NewQuestionForm />} />
-            <Route path={`${urls.question}/:id`} exact render={() => <Question />} />
-            <Route path={`${urls.user}/:id`} exact render={() => <Profile />} />
-            <Route component={NoPage} />
-          </Switch>
-        </Grid>
+                </Suspense>} />
+              <Route path={urls.login} render={() =>
+                <Suspense fallback="loading">
+                  <SignIn setUser={setUser} />
+                </Suspense>} />
+              <Route path={urls.register} render={
+                <Suspense fallback="loading">
+                  <SignupForm />
+                </Suspense>
+              } />
+              <Route path={urls.newQuestion} exact render={() => <NewQuestionForm />} />
+              <Route path={`${urls.question}/:id`} exact render={() => <Question />} />
+              <Route path={`${urls.user}/:id`} exact render={() => <Profile />} />
+              <Route component={NoPage} />
+            </Switch>
+          </Grid>
       </UserContext.Provider>
       <Grid className={classes.footer}>
         <Footer />
